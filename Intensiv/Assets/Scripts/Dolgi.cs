9aa739935f3a25a3c76c390bbe118d69ed0a309f
Garabumba <kostya1473@gmail.com>
2021-12-01 16:52:00 +0300

    Add new scenes

--- Intensiv/Assets/Scripts/Dolgi.cs
+++ Intensiv/Assets/Scripts/Dolgi.cs
@@ -9,6 +9,14 @@ public class Dolgi : MonoBehaviour
     public VoskSpeechToText VoskSpeechToText;
     public Button btn, btn1;
     public Text t;
+    private string sam = "сделать все самому";
+    private string help = "попросить помощи у одногруппников";
+    ScenesManager sm = new ScenesManager();
+
+    private void Awake()
+    {
+        VoskSpeechToText.OnTranscriptionResult += OnTranscriptionResult;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +31,31 @@ public class Dolgi : MonoBehaviour
         {
             btn.gameObject.SetActive(true);
             btn1.gameObject.SetActive(true);
+        }
+    }
+
+    public void MakeAll()
+    {
+        sm.NextScene(6);
+    }
+
+    public void Help()
+    {
+        sm.NextScene(5);
+    }
+    private void OnTranscriptionResult(string obj)
+    {
+        var result = new RecognitionResult(obj);
+        foreach (RecognizedPhrase p in result.Phrases)
+        {
+            if (p.Text == sam && t.GetComponent<PrintedText>().textEnd)
+                MakeAll();
+            if (p.Text == help && t.GetComponent<PrintedText>().textEnd)
+                Help();
+            if (result.Phrases.Length > 0 && result.Phrases[0].Text != "")
+            {
+                t.text = p.Text;
+            }
         }
     }
 }
